a14647b7a48e9ed7d0a8cdf873342caba74ff0cc
Yasir Shoaib <yasirshoaib@hotmail.com>
2018-01-07 22:13:44 -0500

    Save postscript from Lqn model/result
    this will an image reprsentation of the model

--- src/main/java/ca/appsimulations/jlqninterface/utilities/FileHandler.java
+++ src/main/java/ca/appsimulations/jlqninterface/utilities/FileHandler.java
@@ -8,10 +8,14 @@ package ca.appsimulations.jlqninterface.utilities;
  * For details regarding these LQN classes and members refer to LQNS User Manual.
  */
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.SystemUtils;
 
 import java.io.File;
+import java.io.IOException;
 
+@Slf4j
 public class FileHandler {
 
     public static boolean fileExists(String filePath) {
@@ -30,6 +34,31 @@ public class FileHandler {
 
     public static File getResourceFile(String resourceName) throws Exception {
         return new File(FileHandler.class.getClassLoader().getResource(resourceName).toURI());
+    }
+
+    public static void openFile(String filePath) {
+        if (SystemUtils.IS_OS_WINDOWS) {
+            log.info("openFile() is not supported for windows");
+            return;
+        }
+        boolean result = false;
+        StringBuilder strCmd = new StringBuilder();
+        strCmd.append("open ").append(filePath);
+
+        try {
+            Process p = Runtime.getRuntime().exec(strCmd.toString());
+            p.waitFor();
+
+            if ((p.exitValue() == 0)) {
+                result = true;
+            }
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/test/java/ca.appsimulations.jlqninterface/SavePostScript.java
+++ src/test/java/ca.appsimulations.jlqninterface/SavePostScript.java
@@ -1,30 +1,42 @@
 package ca.appsimulations.jlqninterface;
 
+import ca.appsimulations.jlqninterface.lqn.model.LqnModel;
 import ca.appsimulations.jlqninterface.lqn.model.handler.LqnSolver;
+import ca.appsimulations.jlqninterface.lqn.model.parser.LqnResultParser;
 import ca.appsimulations.jlqninterface.utilities.FileHandler;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.File;
 
+import static ca.appsimulations.jlqninterface.utilities.FileHandler.getResourceFile;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class SavePostScript {
     private final String INPUT_LQNX = "input.lqnx";
-    private final String OUTPUT_PS = "input.ps";
-    private File outputFile;
-
-    @BeforeMethod
-    void beforeMethod() {
-        outputFile = new File(OUTPUT_PS);
-        outputFile.delete();
-    }
-
 
     @Test
-    public void test() throws Exception {
-        LqnSolver.savePostScript(FileHandler.getResourceFile(INPUT_LQNX).getAbsolutePath(),
-                                 outputFile.getAbsolutePath());
+    public void savePostScriptOfInputLqnModel() throws Exception {
+
+        File outputPs = new File("input.ps");
+        outputPs.delete();
+        LqnSolver.savePostScript(getResourceFile(INPUT_LQNX).getAbsolutePath(),
+                                 outputPs.getAbsolutePath());
+        assertThat(FileHandler.fileExists(outputPs.getAbsolutePath())).isTrue();
+    }
+
+    @Test
+    public void savePostScriptOfOutputLqnModel() throws Exception {
+        File outputFile = new File("test-output.lqxo");
+        File outputPs = new File("test-output.ps");
+        outputFile.delete();
+        outputPs.delete();
+
+        LqnSolver.solveLqns(getResourceFile(INPUT_LQNX).getAbsolutePath(), new LqnResultParser(new LqnModel()),
+                            outputFile.getAbsolutePath());
         assertThat(FileHandler.fileExists(outputFile.getAbsolutePath())).isTrue();
+
+        LqnSolver.savePostScript(outputFile.getAbsolutePath(),
+                                 outputPs.getAbsolutePath());
+        assertThat(FileHandler.fileExists(outputPs.getAbsolutePath())).isTrue();
     }
 }
